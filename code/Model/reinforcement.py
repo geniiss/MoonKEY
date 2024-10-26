749bd71f869ffa12a39491585b12ead02f2dc4c6
Arnau Perich Iglesias <arnau.perichiglesias@gmail.com>
2024-10-27 01:09:11 +0200

    openai

---
+++ code/Model/reinforcement.py
@@ -0,0 +1,30 @@
+import openai
+import pandas as pd
+
+openai.api_key = "sk-2XANZD8_XcvOI1xXNFMViVVAP576EzQEKOKr99JM9QT3BlbkFJaDgjE2Grw_RlZXUGBe0rSVKo0-WzP96MTiwJGGP9QA"
+dataset = pd.read_csv('./../../data/dataset.csv')
+
+def table_input(dataset, user_id):
+    dataset = dataset[dataset["userid"] == user_id]
+    dataset = dataset[dataset["userid"] == user_id]
+    dataset["is_worst_activity"] = dataset.groupby("aula_id")["activitat"].transform(lambda x: x == x.min())
+    dataset = dataset[dataset["is_worst_activity"] == True]
+    dataset[["aula_id", "P_Grade", "F_Grade", "R_Grade", "activitat", "mark"]]
+    dataset = dataset.rename(columns={
+        'aula_id': 'Subject',
+        'activitat': 'Activity_Name',
+        'mark': 'Activity_Grade'
+    })
+    dataset = dataset[["Subject", "P_Grade", "F_Grade", "R_Grade", "Activity_Name", "Activity_Grade"]]
+    return dataset.to_string(max_rows=None, max_cols=None)
+
+response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are a virtual assistant providing academic reinforcement advice. Your role is, based on a data table containing the Subject, grades from the midterm exam, final exam, and retake (P_Grade, F_Grade, R_Grade), as well as all activities (Activity) and their grades (Activity_Grade), to decide which subject requires reinforcement and recommend three points for improvement."},
+        {"role": "user", "content": table_input(dataset, 789)}
+    ]
+)
+
+print(response.choices[0].message["content"])
+
