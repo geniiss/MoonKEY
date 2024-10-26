dd8f25f304a0438b025c741e3c0bda3d6e3530ae
Arnau Perich Iglesias <arnau.perichiglesias@gmail.com>
2024-10-27 01:33:28 +0200

    reinf

--- code/App/Screens/TrainingActivitiesScreen.py
+++ code/App/Screens/TrainingActivitiesScreen.py
@@ -1,7 +1,17 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Model')))
+from reinforcement import reinforcment_advice
+import pandas as pd
 
 Builder.load_file("kv_files/TrainingActivitiesScreen.kv")
+dataset = pd.read_csv('./../../data/dataset.csv')
 
 class TrainingActivitiesScreen(Screen):
-    pass
+    card_text = StringProperty(reinforcment_advice(dataset, 789))  
+
+    def update_text(self, new_text):
+        self.card_text = new_text  
--- code/Model/reinforcement.py
+++ code/Model/reinforcement.py
@@ -2,9 +2,8 @@ import openai
 import pandas as pd
 
 openai.api_key = "sk-2XANZD8_XcvOI1xXNFMViVVAP576EzQEKOKr99JM9QT3BlbkFJaDgjE2Grw_RlZXUGBe0rSVKo0-WzP96MTiwJGGP9QA"
-dataset = pd.read_csv('./../../data/dataset.csv')
 
-def table_input(dataset, user_id):
+def reinforcment_advice(dataset, user_id):
     dataset = dataset[dataset["userid"] == user_id]
     dataset = dataset[dataset["userid"] == user_id]
     dataset["is_worst_activity"] = dataset.groupby("aula_id")["activitat"].transform(lambda x: x == x.min())
@@ -16,15 +15,15 @@ def table_input(dataset, user_id):
         'mark': 'Activity_Grade'
     })
     dataset = dataset[["Subject", "P_Grade", "F_Grade", "R_Grade", "Activity_Name", "Activity_Grade"]]
-    return dataset.to_string(max_rows=None, max_cols=None)
+    table = dataset.to_string(max_rows=None, max_cols=None)
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a virtual assistant providing academic reinforcement advice. Your role is, based on a data table containing the Subject, grades from the midterm exam, final exam, and retake (P_Grade, F_Grade, R_Grade), as well as all activities (Activity) and their grades (Activity_Grade), to decide which subject requires reinforcement and recommend three points for improvement."},
+            {"role": "user", "content": table}
+        ]
+    )
+    return response.choices[0].message["content"]
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": "You are a virtual assistant providing academic reinforcement advice. Your role is, based on a data table containing the Subject, grades from the midterm exam, final exam, and retake (P_Grade, F_Grade, R_Grade), as well as all activities (Activity) and their grades (Activity_Grade), to decide which subject requires reinforcement and recommend three points for improvement."},
-        {"role": "user", "content": table_input(dataset, 789)}
-    ]
-)
-
-print(response.choices[0].message["content"])
+    
 
