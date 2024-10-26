dccd3ea7cd685706d698dc044fcf0d9e8778082a
joan.bejarano <bejmo24@gmail.com>
2024-10-26 18:12:32 +0200

    HomeScreen mobile resolution

--- code/App/Screens/HomeScreen.py
+++ code/App/Screens/HomeScreen.py
@@ -1,7 +1,22 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 Builder.load_file("kv_files/HomeScreen.kv")
-
 class HomeScreen(Screen):
-    pass
+    # Define una propiedad para la fuente de la imagen
+    image_source = StringProperty("")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_image(2)  # Llama a la función para establecer la imagen inicial
+
+    def set_image(self, imagen):
+        # Cambia la fuente de la imagen según el valor de la variable
+        if imagen == 1:
+            self.image_source = "../../image/monkeys/happy.png"
+        elif imagen == 2:
+            self.image_source = "../../image/monkeys/sad.png"
+        elif imagen == 3:
+            self.image_source = "../../image/monkeys/angry.png"
+        # Puedes añadir más condiciones según tus necesidades
