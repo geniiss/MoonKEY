795fb4ad44e5bc03245a015c92dc0f3283faf1a9
geniiss <genisferrete@gmail.com>
2024-10-26 15:34:19 +0200

    refactor dels .kv

--- code/App/Screens/RankingScreen.py
+++ code/App/Screens/RankingScreen.py
@@ -1,4 +1,7 @@
 from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file("kv_files/RankingScreen.kv")
 
 class RankingScreen(Screen):
     pass
--- code/App/Screens/StatisticsScreen.py
+++ code/App/Screens/StatisticsScreen.py
@@ -1,4 +1,7 @@
 from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file("kv_files/StatisticsScreen.kv")
 
 class StatisticsScreen(Screen):
     pass
