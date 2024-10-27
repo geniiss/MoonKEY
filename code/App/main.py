e3848e97c724c658cd38abccaa73f4fa22a4c578
joan.bejarano <bejmo24@gmail.com>
2024-10-27 10:49:42 +0100

    Animación de seleccionar el menú

--- code/App/main.py
+++ code/App/main.py
@@ -11,6 +11,8 @@ from Screens.RankingScreen import RankingScreen
 from Screens.StatisticsScreen import StatisticsScreen
 from Screens.TrainingActivitiesScreen import TrainingActivitiesScreen
 
+
+
 Header = {
     "home": "MoonKEY",
     "ranking": "Ranking",
@@ -22,8 +24,27 @@ class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Brown"
         return Builder.load_file("./main.kv")
-
+        
+    def delete_styles(self):
+        self.root.ids.home_item.font_style = "Body1";
+        self.root.ids.ranking_item.font_style = "Body1";
+        self.root.ids.statistics_item.font_style = "Body1";
+        self.root.ids.training_activities_item.font_style = "Body1";
+    
     def menu_callback(self, screen_name):
+        # Restablecer todos los estilos a 'Body1'
+        self.delete_styles()
+
+        # Solo aplica el estilo 'H6' al elemento seleccionado
+        if screen_name == "home":
+            self.root.ids.home_item.font_style = "H6"
+        elif screen_name == "ranking":
+            self.root.ids.ranking_item.font_style = "H6"
+        elif screen_name == "statistics":
+            self.root.ids.statistics_item.font_style = "H6"
+        elif screen_name == "training_activities":
+            self.root.ids.training_activities_item.font_style = "H6"
+
         # Cambia a la pantalla seleccionada
         self.root.ids.top_bar.title = Header[screen_name]
         self.root.ids.screen_manager.current = screen_name
